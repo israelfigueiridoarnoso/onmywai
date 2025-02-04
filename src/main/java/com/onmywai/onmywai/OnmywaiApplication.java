e5bc367870a9f1a0a6197ae27ea5f8f762160389
Israel Figueirido <israel.figueirido.arnoso@gmail.com>
2025-02-04 23:10:22 +0100

    Spring Initializr + application.properties setup

---
+++ src/main/java/com/onmywai/onmywai/OnmywaiApplication.java
@@ -0,0 +1,13 @@
+package com.onmywai.onmywai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnmywaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnmywaiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/onmywai/onmywai/OnmywaiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onmywai.onmywai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnmywaiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
